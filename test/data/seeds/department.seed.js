e5ddbe2ecd0efc2909109927e7ff00e17000fec6
okunishinishi <okunishinishi@gmail.com>
2013-10-05 19:38:39 +0900

    add test data

---
+++ test/data/seeds/department.seed.js
@@ -0,0 +1,15 @@
+Object.prototype.repeat = function (count) {
+    this._repeat = count;
+    return this;
+};
+
+module.exports = {
+    /** データ定義 **/
+    entries: [
+        {
+            _id: "1${padZero(rownum, 23)}",
+            client_id: "${padZero(parseInt(rownum/5), 24)}",
+            name: '${fruit}'
+        }.repeat(50)
+    ]
+};
