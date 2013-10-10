90b5081528ee78bda2bb803de0442ae58af885e1
okunishinishi <okunishinishi@gmail.com>
2013-10-10 19:53:48 +0900

    add sort

--- public/javascripts/p.chart.js
+++ public/javascripts/p.chart.js
@@ -3,6 +3,65 @@
         li: Handlebars.templates['chart-list-item']
     };
     $.fn.extend({
+        tableData: function () {
+            var table = $(this),
+                thead = table.find('thead'),
+                tbody = table.find('tbody'),
+                result = {
+                    head: [],
+                    body: []
+                };
+            thead.find('tr').find('th').each(function () {
+                var cell = $(this),
+                    text = cell.text() || '';
+                result.head.push(text);
+            });
+            tbody.find('tr').each(function () {
+                var tr = $(this);
+                var data = [];
+                tr.find('th,td').each(function () {
+                    var cell = $(this),
+                        text = cell.text() || '';
+                    data.push(text);
+                });
+                result.body.push(data);
+            });
+            return result;
+        },
+        sortableTable: function () {
+            var table = $(this),
+                thead = table.find('thead'),
+                tbody = table.find('tbody');
+            $('th', thead).addClass('sortable-th').click(function () {
+                var th = $(this),
+                    index = th.prevAll('th').size() || 0,
+                    asc = th.data('asc');
+                var data = table.tableData()['body'],
+                    bodyHTML = '';
+                data
+                    .sort(function (a, b) {
+                        return a[index].localeCompare(b[index]) * (asc ? 1 : -1);
+                    })
+                    .forEach(function (data) {
+                        var rowHtml = "<tr>";
+                        data.forEach(function (data) {
+                            rowHtml += ["<td>", data , "</td>"].join('');
+                        });
+                        rowHtml += "</tr>";
+                        bodyHTML += rowHtml;
+                    });
+                if (asc) {
+                    th.addClass('asc')
+                        .removeClass('desc');
+                } else {
+                    th.addClass('desc').removeClass('asc');
+                }
+                th.siblings().removeClass('asc').removeClass('desc');
+                th.data('asc', !asc);
+                tbody.html(bodyHTML);
+            }).prepend('<i class="icon icon-chevron-up"></i><i class="icon icon-chevron-down"></i>');
+            return table;
+        },
         chartListTable: function () {
             var table = $(this),
                 thead = table.find('thead'),
@@ -19,6 +78,7 @@
 
                 }
             });
+            table.sortableTable();
             return table;
         }
     });
