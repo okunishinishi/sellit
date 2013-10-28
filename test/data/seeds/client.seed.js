f18b80bf51625d7bdd846bc79f4578738b9c8859
okunishinishi <okunishinishi@gmail.com>
2013-10-28 12:55:02 +0900

    add data

--- test/data/seeds/client.seed.js
+++ test/data/seeds/client.seed.js
@@ -10,8 +10,7 @@ module.exports = {
             _id: "${padZero(rownum, 24)}",
             name: '${flower}${choice("商事,コーポレーション,株式会社,グループ".split(","))}',
             parent_id: '',
-            children_ids: "${choice(null,null,'[]')}",
-            product_ids: "2${padZero(parseInt(rownum%10), 23)},2${padZero(parseInt(rownum/8), 23)}"
+            children_ids: "${choice(null,null,'[]')}"
         }.repeat(10)
     ]
 };
