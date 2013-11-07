ed58f7a4f73539b19d2dc9411f57c0ca750bb9eb
okunishinishi <okunishinishi@gmail.com>
2013-11-08 00:01:12 +0900

    bugfix

--- public/javascripts/templates.js
+++ public/javascripts/templates.js
@@ -31,7 +31,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "\">\n\n    <label class='chart-cell-label' data-value=\"";
+    + "\"\n        target=\"_blank\">\n\n    <label class='chart-cell-label' data-value=\"";
   if (stack1 = helpers.initial_provider) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.initial_provider; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
