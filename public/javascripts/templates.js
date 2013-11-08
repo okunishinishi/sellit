8fe5e62461cd52088a8ecc55de02b27cf369ae04
okunishinishi <okunishinishi@gmail.com>
2013-11-08 12:38:02 +0900

    modify design

--- public/javascripts/templates.js
+++ public/javascripts/templates.js
@@ -109,7 +109,7 @@ function program1(depth0,data) {
   if (stack1 = helpers.href) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.href; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "\">";
+    + "\" target=\"_blank\">";
   if (stack1 = helpers.text) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.text; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
